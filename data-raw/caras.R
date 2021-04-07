3b5e4decb9603faa9da49e578becf03f91b25873
toninif <fernandotonini11@gmail.com>
2021-04-07 17:39:13 -0300

    Primer commit

---
+++ data-raw/caras.R
@@ -0,0 +1,38 @@
+# Datos de caras -----------
+
+# Paquetes
+
+library(tidyverse)
+
+
+# Cargando datos -----------------
+
+
+
+caras <- read_csv("data-raw/data_agg_sim.csv") # Obtenido de https://osf.io/ybws4/download
+
+
+# Preparando la base ------------
+
+caras <- caras %>% select(Region = region, Edad = stim_age, Sexo = stim_sex,
+                          Agresividad = aggressive, Atractividad = attractive,
+                          Amabilidad = caring, Seguridad = confident,
+                          Dominancia = dominant, Emocionalidad = emostable,
+                          Inteligencia = intelligent, Maldad = mean, Vejez = old,
+                          Responsabilidad = responsible, Sociabilidad = sociable,
+                          Confiable = trustworthy, Infelicidad = unhappy, Raro = weird)
+
+caras$Sexo[caras$Sexo == "male"] <- "Hombre"
+caras$Sexo[caras$Sexo == "female"] <- "Mujer"
+caras$Edad <- round(caras$Edad)
+caras$Region[caras$Region == "Australia & New Zealand"] <- "Australia y Nueva Zelanda"
+caras$Region[caras$Region == "Central America & Mexico"] <- "America Central y Mexico"
+caras$Region[caras$Region == "Eastern Europe"] <- "Europa Oriental"
+caras$Region[caras$Region == "Middle East"] <- "Medio Oriente"
+caras$Region[caras$Region == "Scandanavia"] <- "Escandinavia"
+caras$Region[caras$Region == "South America"] <- "America del Sur"
+caras$Region[caras$Region == "UK"] <- "Reino Unido"
+caras$Region[caras$Region == "USA & Canada"] <- "EE.UU y Canada"
+caras$Region[caras$Region == "Western Europe"] <- "Europa Occidental"
+
+
